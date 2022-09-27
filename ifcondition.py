066e9b6a9b652a404246deece77903a475042746
SakshiSujediya <91109734+SakshiSujediya@users.noreply.github.com>
2022-09-27 18:35:43 +0530

    ifcondition

---
+++ ifcondition.py
@@ -0,0 +1,33 @@
+#if condition
+num=2
+if num>0 :
+    print(num,"num is a positive number")
+    print("This is always printed")
+
+num=-1
+if num>0 :
+    print(num,"num is a positive number")
+    print("This is also printed always")
+
+
+#if..else condition (check positive or negative number)
+
+num = -10
+num =5
+ 
+if num>=0:
+    print("Positive or Zero")
+else :
+    print("Negative number")
+
+#if..elif..else (check number positive,negative or zero)
+
+num =0
+num1=-10
+
+if num>10:
+    print("num is postive")
+elif num==1:
+    print("num value in 0")
+else:
+    print("Print num is negative")
