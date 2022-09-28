d7ae3f64656da0bfa5a0edd4d992b502eff98322
SakshiSujediya <91109734+SakshiSujediya@users.noreply.github.com>
2022-09-28 17:08:32 +0530

    breakcondition

---
+++ break&continue.py
@@ -0,0 +1,17 @@
+#Break statement
+
+for val in "String" :
+    if val == "i" :
+      break
+    print(val)
+
+print("The end")
+
+
+
+#continue statement
+for val in "String" :
+    if val =="i" :
+        continue
+    print(val)
+print("The end")
