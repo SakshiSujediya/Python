94b12d5af187014ddc084d9d8654e499573b3acf
SakshiSujediya <91109734+SakshiSujediya@users.noreply.github.com>
2022-10-03 18:52:07 +0530

    functions

---
+++ add&multiple numberfun.py
@@ -0,0 +1,16 @@
+def add_number(num1,num2):
+
+     result =num1 + num2
+     
+     print("sum of no",result)
+     
+def add_multiple(m1,m2):   
+    mresult =m1 * m2 
+    print("multiple of no is",mresult)  
+
+n1=5
+n2=10
+m1=5
+m2=2
+add_number(n1,n2)
+add_multiple(m1,m2)      
