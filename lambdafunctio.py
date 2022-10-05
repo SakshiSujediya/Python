84f80e4f43e50413f8be31f62517547e8839b4c2
SakshiSujediya <91109734+SakshiSujediya@users.noreply.github.com>
2022-10-05 18:53:55 +0530

    fun

--- lambdafunctio.py
+++ lambdafunctio.py
@@ -1,13 +1,7 @@
 #Adding number by user lambda function
-from http.client import _DataType
+
 
 
 Add = lambda a:a+5
 print(Add(10))
 
-#print datatype
-
-Types = lambda num : num+ e if (num + int)  else  (num +2.f) :
-   
-print("print the value of num + e",num +5)
-print("print the value of num + e",num +2.f)
