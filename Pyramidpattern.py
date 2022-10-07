00231d2e9904e88eda69627a1b9f2366f7d44f86
SakshiSujediya <91109734+SakshiSujediya@users.noreply.github.com>
2022-10-07 19:07:50 +0530

    star pattern

---
+++ Pyramidpattern.py
@@ -0,0 +1,7 @@
+for i in range(0,8):
+    for j in range(0,i-1):
+        print(" ",end="")
+        for k in range (0,2*i-1):
+            print("*")
+    
+    print("\r")
--- Starpattern.py
+++ Starpattern.py
@@ -1,5 +1,5 @@
 #simple star patter(5)
-num=8
+num=5
 for i in range(0,num) :
     for j in range(0,i+1) :
        print("* ",end="")
