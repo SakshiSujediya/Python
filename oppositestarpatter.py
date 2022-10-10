3785161732c1200efb679764791e35140a51413b
SakshiSujediya <91109734+SakshiSujediya@users.noreply.github.com>
2022-10-10 18:39:03 +0530

    Patterns

--- oppositestarpatter.py
+++ oppositestarpatter.py
@@ -6,3 +6,12 @@ for i in range(0,4):
         print("*", end=" ")
     print("\r")      
 
+#right side Triangle
+for i in range (0,5):
+    for j in range(0,5-i):
+         print(" ",end=" ")
+
+    for k in range(0,i+1):
+        print("* ",end="")
+
+    print("\r")    
